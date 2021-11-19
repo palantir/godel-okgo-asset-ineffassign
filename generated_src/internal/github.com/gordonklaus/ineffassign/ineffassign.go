341e0276b1b08ea236dcf326b5f99de91c6a241f
Nick Miyake <nmiyake@palantir.com>
2021-11-19 07:51:25 -0800

    Combine Go version upgrade with amalgomate plugin upgrade

---
+++ generated_src/internal/github.com/gordonklaus/ineffassign/ineffassign.go
@@ -0,0 +1,16 @@
+package amalgomated
+
+import (
+	"github.com/palantir/godel-okgo-asset-ineffassign/generated_src/internal/amalgomated_flag"
+
+	"github.com/palantir/godel-okgo-asset-ineffassign/generated_src/internal/github.com/gordonklaus/ineffassign/pkg/ineffassign"
+	"golang.org/x/tools/go/analysis/singlechecker"
+)
+
+func AmalgomatedMain() {
+	singlechecker.Main(ineffassign.Analyzer)
+}
+
+func init() {
+	flag.Bool("n", false, "don't recursively check paths (deprecated)")
+}
